12a179ca56499212432cdf47275b4f5601ce1d8a
Arya Irama Wahono <aryairama987@gmail.com>
2021-09-09 04:41:57 +0700

    integerasi orders

---
+++ src/redux/action/orderAction.js
@@ -0,0 +1,20 @@
+import axios from '../../config/Axios';
+import { toast } from 'react-toastify';
+
+export const createOrder = (carts, formOrder, totalPrice, router) => async (dispatch) => {
+  try {
+    await axios.post('/orders', {
+      phone_number: formOrder.phone_number,
+      address: formOrder.address,
+      payment: formOrder.payment,
+      carts,
+      cartOrders: carts,
+      total_price: totalPrice,
+    });
+    router.push('/products');
+    toast.success('Successful Order Product');
+    dispatch({ type: 'CART_MUTLI', payload: [] });
+  } catch (error) {
+    console.log(error);
+  }
+};
