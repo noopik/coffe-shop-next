de75c2b9ce6044e1c0c58f55f458794be48fe837
dwinovic <nd.cahya19@gmail.com>
2021-09-08 23:03:19 +0700

    add button submit in cart page

--- pages/carts/index.js
+++ pages/carts/index.js
@@ -1,14 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
-import Pagination from '@material-ui/lab/Pagination';
-import Image from 'next/image';
-import { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import { IMG_BGHistoryPage, IMG_DummyProductCard } from '../../src/assets';
-import PrivateRoute from '../../src/components/hoc/PrivateRoute';
-import { ModalAlertValidation } from '../../src/components/molecules';
-import { Breakpoints } from '../../src/utils';
+import router from 'next/router';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { deleteOrderCart, decQuantity, incQuantity } from '../../src/redux/action/cartAction';
+import styled from 'styled-components';
+import { Button } from '../../src/components';
+import PrivateRoute from '../../src/components/hoc/PrivateRoute';
+import {
+  decQuantity,
+  deleteOrderCart,
+  incQuantity,
+} from '../../src/redux/action/cartAction';
+import { Breakpoints } from '../../src/utils';
 
 const CartsPage = () => {
   // const [page, setPage] = useState(1);
@@ -21,7 +23,12 @@ const CartsPage = () => {
   const { cart_multi } = useSelector((state) => state.cart);
   return (
     <StyledHistoryPage>
-      <Image src={IMG_BGHistoryPage} alt="background page" layout="fill" className="bg-page" />
+      {/* <Image
+        src={IMG_BGHistoryPage}
+        alt="background page"
+        layout="fill"
+        className="bg-page"
+      /> */}
       <div className="container">
         <h1 className="heading">Carts</h1>
         <p className="sub-heading">List of carts you have</p>
@@ -31,17 +38,28 @@ const CartsPage = () => {
               <div
                 className="item"
                 key={index}
-                onClick={() => (selected === index ? setSelected('') : setSelected(index))}
+                onClick={() =>
+                  selected === index ? setSelected('') : setSelected(index)
+                }
               >
                 <div className="image-product">
-                  <img src={`${process.env.NEXT_PUBLIC_API_URL}/${cartValue.product_img_product}`} alt="name product" />
+                  <img
+                    src={`${process.env.NEXT_PUBLIC_API_URL}/${cartValue.product_img_product}`}
+                    alt="name product"
+                  />
                 </div>
                 <div className="desc">
                   <h4 className="title-product">{cartValue.product_name}</h4>
                   <p className="text">IDR {cartValue.total_price}</p>
-                  <p className="text-utility">Quantity : {cartValue.cart_stock}</p>
-                  <p className="text-utility">Size : {cartValue.card_size_name}</p>
-                  <p className="text-utility">Delivery method : {cartValue.cart_deliver_name}</p>
+                  <p className="text-utility">
+                    Quantity : {cartValue.cart_stock}
+                  </p>
+                  <p className="text-utility">
+                    Size : {cartValue.card_size_name}
+                  </p>
+                  <p className="text-utility">
+                    Delivery method : {cartValue.cart_deliver_name}
+                  </p>
                 </div>
                 {selected === index && (
                   <div className="btn-wrapper">
@@ -49,36 +67,87 @@ const CartsPage = () => {
                       className="btn delete"
                       onClick={() => {
                         dispatch(
-                          decQuantity(cartValue.cart_product_id, cartValue.cart_deliver_id, cartValue.cart_size_id)
+                          decQuantity(
+                            cartValue.cart_product_id,
+                            cartValue.cart_deliver_id,
+                            cartValue.cart_size_id
+                          )
                         );
                       }}
                     >
-                      <svg width="24" height="4" viewBox="0 0 24 4" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect x="24" width="4" height="24" rx="2" transform="rotate(90 24 0)" fill="white" />
+                      <svg
+                        width="24"
+                        height="4"
+                        viewBox="0 0 24 4"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <rect
+                          x="24"
+                          width="4"
+                          height="24"
+                          rx="2"
+                          transform="rotate(90 24 0)"
+                          fill="white"
+                        />
                       </svg>
                     </div>
                     <div
                       className="btn delete"
                       onClick={() => {
                         dispatch(
-                          incQuantity(cartValue.cart_product_id, cartValue.cart_deliver_id, cartValue.cart_size_id)
+                          incQuantity(
+                            cartValue.cart_product_id,
+                            cartValue.cart_deliver_id,
+                            cartValue.cart_size_id
+                          )
                         );
                       }}
                     >
-                      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect x="10" width="4" height="24" rx="2" fill="white" />
-                        <rect x="24" y="10" width="4" height="24" rx="2" transform="rotate(90 24 10)" fill="white" />
+                      <svg
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <rect
+                          x="10"
+                          width="4"
+                          height="24"
+                          rx="2"
+                          fill="white"
+                        />
+                        <rect
+                          x="24"
+                          y="10"
+                          width="4"
+                          height="24"
+                          rx="2"
+                          transform="rotate(90 24 10)"
+                          fill="white"
+                        />
                       </svg>
                     </div>
                     <div
                       className="btn close"
                       onClick={() => {
                         dispatch(
-                          deleteOrderCart(cartValue.cart_product_id, cartValue.cart_deliver_id, cartValue.cart_size_id)
+                          deleteOrderCart(
+                            cartValue.cart_product_id,
+                            cartValue.cart_deliver_id,
+                            cartValue.cart_size_id
+                          )
                         );
                       }}
                     >
-                      <svg width="23" height="24" viewBox="0 0 23 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <svg
+                        width="23"
+                        height="24"
+                        viewBox="0 0 23 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
                         <path
                           d="M2 6H4.11111M4.11111 6H21M4.11111 6V20C4.11111 20.5304 4.33353 21.0391 4.72944 21.4142C5.12535 21.7893 5.66232 22 6.22222 22H16.7778C17.3377 22 17.8746 21.7893 18.2706 21.4142C18.6665 21.0391 18.8889 20.5304 18.8889 20V6H4.11111ZM7.27778 6V4C7.27778 3.46957 7.5002 2.96086 7.89611 2.58579C8.29202 2.21071 8.82899 2 9.38889 2H13.6111C14.171 2 14.708 2.21071 15.1039 2.58579C15.4998 2.96086 15.7222 3.46957 15.7222 4V6M9.38889 11V17M13.6111 11V17"
                           stroke="#6A4029"
@@ -97,15 +166,13 @@ const CartsPage = () => {
       </div>
       <div className="pagination">
         {/* <Pagination className="page" count={10} page={page} onChange={handlePagination} /> */}
+        <Button
+          className="button-action"
+          onClick={() => router.push('/orders')}
+        >
+          Submit
+        </Button>
       </div>
-      <ModalAlertValidation
-        show={showModal}
-        onClose={() => setShowModal(false)}
-        actionDelete={() => {
-          console.log('delete item 1');
-          // setShowModal(false);
-        }}
-      />
     </StyledHistoryPage>
   );
 };
@@ -115,25 +182,11 @@ export default PrivateRoute(CartsPage, ['member', 'admin']);
 // START === STYLING CURRENT PAGE
 
 const StyledHistoryPage = styled.div`
-  /* START == BREAKPOINT */
-  /* ${Breakpoints.lessThan('2xl')`
-      background-color: yellow;
-    `}
-  ${Breakpoints.lessThan('xl')`
-      background-color: blue;
-    `}
-    ${Breakpoints.lessThan('lg')`
-      background-color: cyan;
-    `}
-    ${Breakpoints.lessThan('md')`
-      background-color: pink;
-    `}
-    ${Breakpoints.lessThan('sm')`
-      background-color: green;
-    `}
-    ${Breakpoints.lessThan('xsm')`
-      background-color: pink;
-    `} */
+  background-image: url('/HeaderHomePage.png');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  background-size: cover;
   position: relative;
   padding-top: 150px;
   img {
@@ -183,11 +236,13 @@ const StyledHistoryPage = styled.div`
       position: relative;
       .image-product {
         position: relative;
-        width: 75px;
-        height: 75px;
+        width: 120px;
+        height: 120px;
         img {
           object-fit: cover;
           z-index: 1;
+          width: 100%;
+          height: 100%;
           border-radius: 100%;
         }
       }
@@ -248,14 +303,36 @@ const StyledHistoryPage = styled.div`
   }
   .pagination {
     margin-top: 50px;
-    padding: 16px 0;
     display: flex;
+    padding: 16px 0;
     justify-content: center;
     background-color: #ffffff;
-    .page {
-      ul li button {
-        font-size: 18px;
-      }
+    z-index: 9;
+    ${Breakpoints.lessThan('sm')`
+      width: 100%;
+      padding: 16px;
+    `}
+
+    /* ${Breakpoints.lessThan('2xl')`
+      background-color: yellow;
+    `}
+    ${Breakpoints.lessThan('xl')`
+      background-color: blue;
+    `}
+    ${Breakpoints.lessThan('lg')`
+      background-color: cyan;
+    `}
+    ${Breakpoints.lessThan('md')`
+      background-color: pink;
+    `}
+    ${Breakpoints.lessThan('sm')`
+      background-color: green;
+    `}
+    ${Breakpoints.lessThan('xsm')`
+      background-color: pink;
+    `} */
+    .button-action {
+      width: 500px;
     }
   }
 `;
