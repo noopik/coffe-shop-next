ad46b1ae9b73867efd82dff8827c4c998253e347
Arya Irama Wahono <aryairama987@gmail.com>
2021-09-08 20:28:14 +0700

    update limit pagination,update style navbar

--- pages/products/index.js
+++ pages/products/index.js
@@ -31,7 +31,7 @@ const ProductsPage = ({ user, auth }) => {
   useEffect(async () => {
     if (categoryId === null) {
       const { data, pagination } = await getProducts(
-        20,
+        10,
         sortASC ? 'ASC' : 'DESC ',
         page,
         router.query.search || '',
@@ -41,7 +41,7 @@ const ProductsPage = ({ user, auth }) => {
     } else if (categoryId !== null) {
       const { data, pagination } = await getProductsById(
         categoryId,
-        20,
+        10,
         sortASC ? 'ASC' : 'DESC ',
         page,
         router.query.search || '',
