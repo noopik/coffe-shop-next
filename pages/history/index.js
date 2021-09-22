7688df18d191cf0b5a8d0bf621800afa1f6b706e
dwinovic <nd.cahya19@gmail.com>
2021-09-22 20:43:10 +0700

    remove cloudinary setup

--- pages/history/index.js
+++ pages/history/index.js
@@ -72,7 +72,7 @@ const HistoryPage = () => {
               >
                 <div className="image-product">
                   <img
-                    src={`${history.img_product}`}
+                    src={`${process.env.NEXT_PUBLIC_API_URL}/${history.img_product}`}
                     alt="name product"
                     layout="fill"
                   />
